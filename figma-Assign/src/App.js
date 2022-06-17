a7f3599c2d7c8d26f6f1c7e0e6f99623fc9b9f98
MJaved9 <101566706+MJaved9@users.noreply.github.com>
2022-06-18 00:28:00 +0530

    Add files via upload

---
+++ figma-Assign/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Figma from "./components/Figma.jsx"
+function App() {
+  return (
+    <div className="App">
+     <Figma/>
+    </div>
+  );
+}
+
+export default App;
